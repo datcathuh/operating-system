9078da3fb50681826201161d619fa670c665da3c
Anonymous <anonymous@gmail.com>
2025-09-27 09:53:44 +0200

    Move the kernel shell to its own file

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -1,55 +1,5 @@
-#include "vga.h"
-#include "keyboard.h"
-#include "string.h"
-
-static const char* _prompt = " > ";
-static const int _prompt_length = sizeof(_prompt) - 1;
-
-static void prompt() {
-	vga_output_pos_set(0, 24);
-	vga_put_string(_prompt);
-	vga_cursor_pos_set(_prompt_length, 24);
-}
+#include "kshell.h"
 
 void kmain(void) {
-    vga_clear();
-    vga_put_string("Hello from os!\n");
-    vga_put_string("t.me/x3ghx \n");
-    vga_put_string_color("color support!\n", White, Magenta);
-
-    prompt();
-
-	char command[70] = "";
-	const int command_size = sizeof(command);
-
-    while (1) {
-        char c = keyboard_get_key();
-        if (c) {
-			if(c == '\n') {
-				vga_clear();
-				command[0] = 0;
-				prompt();
-				continue;
-			}
-			if(c == '\b') {
-				int x,y;
-				vga_output_pos_get(&x, &y);
-				if(x <= _prompt_length) {
-					continue;
-				}
-				command[str_length(command) - 1] = 0;
-				vga_put_char(c);
-				vga_output_pos_get(&x, &y);
-				vga_cursor_pos_set(x, y);
-				continue;
-			}
-
-			if(str_append_char(command, c, command_size)) {
-				vga_put_char(c);
-				int x,y;
-				vga_output_pos_get(&x, &y);
-				vga_cursor_pos_set(x, y);
-			}
-        }
-    }
+	kshell();
 }
