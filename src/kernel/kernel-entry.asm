64db4c8678ae1b6f92e111e799726c133929ce9f
Henrik Nilsson <nsmoooose@gmail.com>
2025-10-25 19:02:18 +0200

    Remove print in the entrypoint

--- src/kernel/kernel-entry.asm
+++ src/kernel/kernel-entry.asm
@@ -2,11 +2,6 @@
 global _start
 
 _start:
-	mov eax, 0xB8000
-    mov byte [eax], 'X'      ; write character
-    mov byte [eax+1], 0x0F   ; attribute (white on black)
-
 	extern kmain
 	call kmain
-	;;// hlt
 	jmp $
