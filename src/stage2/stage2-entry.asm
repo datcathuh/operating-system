34c91075f18a8278eb5b6ecbab50a2eec1b0fa21
Henrik Nilsson <nsmoooose@gmail.com>
2025-12-01 13:48:53 +0100

    Put data in correct section

--- src/stage2/stage2-entry.asm
+++ src/stage2/stage2-entry.asm
@@ -2,11 +2,14 @@
 global start
 
 extern disk_load
+extern gdt_descriptor
 extern print_string
 extern print_new_line
 
 KERNEL_16_SEGMENT equ 0x1000
 KERNEL_16_OFFSET  equ 0x0000
+CODE_SEG equ 0x08
+DATA_SEG equ 0x10
 
 %include "kernel_sector_count.asm"
 
@@ -66,14 +69,13 @@ start_32bit:
 	or eax, 1 << 5           ; Set PAE (bit 5)
 	mov cr4, eax
 
-
-
 	extern s2main
 	call s2main
 
 	call 0x100000
 	jmp $
 
+section .data
 msg_stage2_start db 'Stage2 starting', 0
 
 	;; Disk Address Packet
@@ -86,39 +88,6 @@ dap:
 dap_lba_start:
     dq 0                   ; starting LBA (sector number)
 
-;;; gdt_start and gdt_end labels are used to compute size
-
-; null segment descriptor
-gdt_start:
-    dq 0x0
-
-; code segment descriptor
-gdt_code:
-    dw 0xffff    ; segment length, bits 0-15
-    dw 0x0       ; segment base, bits 0-15
-    db 0x0       ; segment base, bits 16-23
-    db 10011010b ; flags (8 bits)
-    db 11001111b ; flags (4 bits) + segment length, bits 16-19
-    db 0x0       ; segment base, bits 24-31
-
-; data segment descriptor
-gdt_data:
-    dw 0xffff    ; segment length, bits 0-15
-    dw 0x0       ; segment base, bits 0-15
-    db 0x0       ; segment base, bits 16-23
-    db 10010010b ; flags (8 bits)
-    db 11001111b ; flags (4 bits) + segment length, bits 16-19
-    db 0x0       ; segment base, bits 24-31
-
-gdt_end:
-
-; GDT descriptor
-gdt_descriptor:
-    dw gdt_end - gdt_start - 1 ; size (16 bit)
-    dd gdt_start ; address (32 bit)
 
 BOOT_DRIVE     db	0
 STAGE2_SIZE    db	0
-
-CODE_SEG equ 0x08
-DATA_SEG equ 0x10
