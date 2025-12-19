d1bba1a24a05d3deaaf0af9b755588ce8fb69530
Henrik Nilsson <nsmoooose@gmail.com>
2025-12-19 17:19:52 +0100

    Clear memory area for BSS and enable SSE types of instructions
    The BSS section isn't cleared by default after the kernel has been
    loaded. This is now done during start of the kernel to ensure that
    memory there is initialized with 0. For this we have added two
    linker variables that we use to know where the BSS exist in memory.
    
    Also enable SSE and SSE2 instructions. These instructions are used
    when you compile the kernel with -O2 and higher optimization level. If
    not enabled this will cause a crash.

--- src/kernel/kernel-entry.asm
+++ src/kernel/kernel-entry.asm
@@ -1,7 +1,42 @@
 [bits 64]
 global _start
 
+extern __bss_start
+extern __bss_end
+
 _start:
+	;; Clear BSS manually (no SSE, no memset)
+	lea rdi, __bss_start
+	lea rcx, __bss_end
+    sub rcx, rdi
+    shr rcx, 3
+    xor rax, rax
+    rep stosq
+
+	call sse_enable
+
 	extern kmain
 	call kmain
 	jmp $
+
+sse_enable:
+	;; Enable SSE / SSE2
+	;; Safe to run in long mode
+	;; Must be executed before any C code that may use XMM registers
+
+    ; --- Enable FPU ---
+    mov     rax, cr0
+    and     rax, ~(1 << 2)     ; CR0.EM = 0 (disable emulation)
+    or      rax,  (1 << 1)     ; CR0.MP = 1 (monitor coprocessor)
+    mov     cr0, rax
+
+    ; --- Enable SSE instructions ---
+    mov     rax, cr4
+    or      rax,  (1 << 9)     ; CR4.OSFXSR = 1 (FXSAVE/FXRSTOR)
+    or      rax,  (1 << 10)    ; CR4.OSXMMEXCPT = 1 (SSE exceptions)
+    mov     cr4, rax
+
+    ; --- Initialize FPU / XMM state ---
+    fninit                     ; or finit
+
+    ret
