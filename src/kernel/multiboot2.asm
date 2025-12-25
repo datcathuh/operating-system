804afc25d2e4ccf39522d142ffc50d1354283f34
Henrik Nilsson <nsmoooose@gmail.com>
2025-12-25 16:23:16 +0100

    Added multiboot2 signature to the kernel
    One of the first steps towards UEFI support.

---
+++ src/kernel/multiboot2.asm
@@ -0,0 +1,30 @@
+bits 32
+
+extern _start
+
+section .text
+align 8
+
+MB2_MAGIC        equ 0xE85250D6
+MB2_ARCH_X86_64  equ 0
+MB2_HEADER_LEN   equ multiboot2_header_end - multiboot2_header_start
+MB2_CHECKSUM     equ -(MB2_MAGIC + MB2_ARCH_X86_64 + MB2_HEADER_LEN)
+
+multiboot2_header_start:
+    dd MB2_MAGIC
+    dd MB2_ARCH_X86_64
+    dd MB2_HEADER_LEN
+    dd MB2_CHECKSUM
+
+    ; --- Entry address tag ---
+    dw 3                  ; type = entry address
+    dw 0
+    dd 12
+    dd _start
+
+    ; --- End tag ---
+    dw 0
+    dw 0
+    dd 8
+
+multiboot2_header_end:
