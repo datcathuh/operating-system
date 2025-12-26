b3be6ff61424365709e43385eba9e49994be1364
Anonymous <anonymous@gmail.com>
2025-12-26 15:00:00 +0100

    Some adjustments regarding UEFI

--- src/kernel/boot/multiboot2_signature.asm
+++ src/kernel/boot/multiboot2_signature.asm
@@ -1,30 +1,28 @@
 bits 32
 
+;; Valuable documentation exist here for the multiboot2 specification:
+;; https://www.gnu.org/software/grub/manual/multiboot2/multiboot.html
+
 extern multiboot2_entry
 
-section .text
-align 8
-
-MB2_MAGIC        equ 0xE85250D6
-MB2_ARCH_X86_64  equ 0
-MB2_HEADER_LEN   equ multiboot2_header_end - multiboot2_header_start
-MB2_CHECKSUM     equ -(MB2_MAGIC + MB2_ARCH_X86_64 + MB2_HEADER_LEN)
+section .multiboot2
 
 multiboot2_header_start:
-    dd MB2_MAGIC
-    dd MB2_ARCH_X86_64
-    dd MB2_HEADER_LEN
-    dd MB2_CHECKSUM
+    dd 0xe85250d6               ; magic number (multiboot 2)
+    dd 0                        ; architecture 0 (protected mode i386)
+    dd multiboot2_header_end - multiboot2_header_start ; header length
+    ; checksum
+    dd 0x100000000 - (0xe85250d6 + 0 + (multiboot2_header_end - multiboot2_header_start))
 
     ; --- Entry address tag ---
-    dw 3                  ; type = entry address
-    dw 0
-    dd 12
+    dw 3                  		; type (entry address)
+    dw 0				  		; flags
+    dd 12				  		; size
     dd multiboot2_entry
 
     ; --- End tag ---
-    dw 0
-    dw 0
-    dd 8
+    dw 0						; type (end)
+    dw 0						; flags
+    dd 8						; size
 
 multiboot2_header_end:
