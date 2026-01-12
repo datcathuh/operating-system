56ab71d4d32ee0fd3418bdb4553f6b2fe45caf26
Henrik Nilsson <nsmoooose@gmail.com>
2026-01-12 16:56:40 +0100

    Parse memorymap from multiboot

--- src/kernel/arch/x86_64/entry/multiboot2.c
+++ src/kernel/arch/x86_64/entry/multiboot2.c
@@ -1,11 +1,43 @@
 #include "memory.h"
 #include "multiboot2.h"
+#include "serial.h"
 
 static struct multiboot2_tag_framebuffer fb = {0};
 static struct multiboot2_tag_acpi *multiboot2_tag_acpi = 0;
 
 static void multiboot2_parse_tag_framebuffer(struct multiboot2_tag *tag) {
 	mem_copy(&fb, tag, sizeof(struct multiboot2_tag_framebuffer));
+}
+
+static void multiboot2_parse_tag_mmap(struct multiboot2_tag *tag) {
+	struct multiboot2_tag_mmap *mmap = (struct multiboot2_tag_mmap *)tag;
+
+	uint64_t entries = (mmap->size - sizeof(*mmap)) / mmap->entry_size;
+
+	uint64_t total_memory = 0;
+	serial_puts("Memory: \n");
+	for (uint64_t i = 0; i < entries; i++) {
+		struct multiboot2_mmap_entry *e =
+			(struct multiboot2_mmap_entry *)((uint8_t *)mmap->entries +
+		                                     i * mmap->entry_size);
+
+		if (e->type == MULTIBOOT2_MEMORY_AVAILABLE) {
+			total_memory += e->len;
+
+			serial_puts("  ");
+			serial_put_hex64(e->addr);
+			serial_puts(": ");
+			serial_put_dec(e->len);
+			serial_puts(" bytes\n");
+
+			/* TODO Add memory add_usable_memory(e->addr, e->len); */
+		}
+	}
+	serial_puts(" bytes\n");
+	serial_puts("  Total memory: ");
+	serial_put_dec(total_memory);
+	serial_puts(" bytes\n");
+	serial_puts("\n");
 }
 
 struct multiboot2_tag_framebuffer *multiboot2_get_framebuffer(void) {
@@ -27,6 +59,9 @@ void multiboot2_parse(void *mb_addr) {
 		switch (tag->type) {
 		case MULTIBOOT2_TAG_TYPE_FRAMEBUFFER:
 			multiboot2_parse_tag_framebuffer(tag);
+			break;
+		case MULTIBOOT2_TAG_TYPE_MMAP:
+			multiboot2_parse_tag_mmap(tag);
 			break;
 		case MULTIBOOT2_TAG_TYPE_ACPI_1:
 		case MULTIBOOT2_TAG_TYPE_ACPI_2:
--- src/kernel/arch/x86_64/entry/multiboot2.h
+++ src/kernel/arch/x86_64/entry/multiboot2.h
@@ -4,13 +4,31 @@
 
 #define MULTIBOOT2_BOOTLOADER_MAGIC 0x36D76289
 #define MULTIBOOT2_TAG_TYPE_END 0
+#define MULTIBOOT2_TAG_TYPE_MMAP 6
 #define MULTIBOOT2_TAG_TYPE_FRAMEBUFFER 8
 #define MULTIBOOT2_TAG_TYPE_ACPI_1 14
 #define MULTIBOOT2_TAG_TYPE_ACPI_2 15
 
+#define MULTIBOOT2_MEMORY_AVAILABLE 1
+
 struct multiboot2_tag {
 	uint32_t type;
 	uint32_t size;
+};
+
+struct multiboot2_mmap_entry {
+	uint64_t addr;
+	uint64_t len;
+	uint32_t type;
+	uint32_t zero;
+};
+
+struct multiboot2_tag_mmap {
+	uint32_t type;
+	uint32_t size;
+	uint32_t entry_size;
+	uint32_t entry_version;
+	struct multiboot2_mmap_entry entries[];
 };
 
 struct multiboot2_tag_framebuffer {
